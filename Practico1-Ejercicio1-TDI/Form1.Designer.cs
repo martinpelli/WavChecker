4ee1d857ca3fe11a1570b7868e7403e1e3854eb8
Pelli <friedstudios@outlook.com>
2021-08-25 09:12:07 -0300

    Interface Created

---
+++ Practico1-Ejercicio1-TDI/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿
+namespace Practico1_Ejercicio1_TDI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tittleLabel = new System.Windows.Forms.Label();
+            this.loadButton = new System.Windows.Forms.Button();
+            this.outputTextBox = new System.Windows.Forms.TextBox();
+            this.openFile = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // tittleLabel
+            // 
+            this.tittleLabel.AutoSize = true;
+            this.tittleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tittleLabel.Location = new System.Drawing.Point(56, 24);
+            this.tittleLabel.Name = "tittleLabel";
+            this.tittleLabel.Size = new System.Drawing.Size(298, 31);
+            this.tittleLabel.TabIndex = 0;
+            this.tittleLabel.Text = "Detector de Audio 3000";
+            // 
+            // loadButton
+            // 
+            this.loadButton.Location = new System.Drawing.Point(139, 99);
+            this.loadButton.Name = "loadButton";
+            this.loadButton.Size = new System.Drawing.Size(121, 35);
+            this.loadButton.TabIndex = 1;
+            this.loadButton.Text = "Cargar";
+            this.loadButton.UseVisualStyleBackColor = true;
+            // 
+            // outputTextBox
+            // 
+            this.outputTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputTextBox.Location = new System.Drawing.Point(77, 152);
+            this.outputTextBox.MinimumSize = new System.Drawing.Size(0, 50);
+            this.outputTextBox.Name = "outputTextBox";
+            this.outputTextBox.ReadOnly = true;
+            this.outputTextBox.Size = new System.Drawing.Size(240, 50);
+            this.outputTextBox.TabIndex = 2;
+            this.outputTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // openFile
+            // 
+            this.openFile.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(402, 268);
+            this.Controls.Add(this.outputTextBox);
+            this.Controls.Add(this.loadButton);
+            this.Controls.Add(this.tittleLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label tittleLabel;
+        private System.Windows.Forms.Button loadButton;
+        private System.Windows.Forms.TextBox outputTextBox;
+        private System.Windows.Forms.OpenFileDialog openFile;
+    }
+}
+
